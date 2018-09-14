bc6e4887aa50204201750db356d3bcfd19eb2501
Daulet Urazalinov <daulet.urazalinov@uptake.com>
2018-09-14 16:22:47 -0400

    Initial commit

---
+++ Udemy VueJs/Exercise 6 - Forms/src/components/FullName.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <label>First Name</label>
+      <input type="text" class="form-control" :value="firstName" @input="valueChanged(true, $event)">
+    </div>
+
+    <div class="col">
+      <label>Last Name</label>
+      <input type="text" class="form-control" :value="lastName" @input="valueChanged(false, $event)">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FullName",
+    props: {
+      value: {
+        type: String
+      }
+    },
+    computed: {
+      firstName() {
+        return this.value.split(' ')[0];
+      },
+      lastName() {
+        return this.value.split(' ')[1];
+      }
+    },
+    methods: {
+      valueChanged(isFirst, {target: { value }}) {
+        this.$emit('input', isFirst ? `${value} ${this.lastName}` : `${this.firstName} ${value}`);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
